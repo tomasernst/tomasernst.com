968a5f8506f7a81acb16b30265761b3d12efa148
tomasernst <ternst@uc.cl>
2024-11-07 18:15:05 -0300

    fix: navbar and text positioning on main screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,24 @@
-// import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
 
-import NavBar from './Components/NavBar.tsx'
+import NavBar from './Components/NavBar.tsx';
 
-import App from './App.tsx'
-import WorldMap from './Components/WorldMap.tsx'
-import BucketList from './Components/BucketList.tsx'
-import Minesweeper from './Components/Minesweeper/Minesweeper.tsx'
+import App from './App.tsx';
+import WorldMap from './Components/WorldMap.tsx';
+import BucketList from './Components/BucketList.tsx';
+import Minesweeper from './Components/Minesweeper/Minesweeper.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
   <Router>
-    <NavBar />  
+    <NavBar />
+    <div className="main-container">
       <Routes>
         <Route path='/' element={ <App /> } />
         <Route path='/worldmap' element={ <WorldMap /> } />
         <Route path='/bucketlist' element={ <BucketList /> } />
         <Route path='/minesweeper' element={ <Minesweeper /> } />
       </Routes>
+    </div>
   </Router>
-  // </StrictMode>,
-)
+);
