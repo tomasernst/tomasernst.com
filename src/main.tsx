b6ba1cc36e86dd022070210d8fe6e929a8dcc5a3
tomasernst <ternst@uc.cl>
2024-11-07 17:10:35 -0300

    feat: routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
+
 import App from './App.tsx'
+import WorldMap from './Components/WorldMap.tsx'
+import BucketList from './Components/BucketList.tsx'
+import Minesweeper from './Components/Minesweeper/Minesweeper.tsx'
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
-    <App />
+  <Router>
+    <Routes>
+      <Route path='/' element={ <App /> } />
+      <Route path='/worldmap' element={ <WorldMap /> } />
+      <Route path='/bucketlist' element={ <BucketList /> } />
+      <Route path='/minesweeper' element={ <Minesweeper /> } />
+    </Routes>
+  </Router>
   // </StrictMode>,
 )
