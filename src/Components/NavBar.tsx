de9d08d9f2968e39269168f5308a8d165eacc634
tomasernst <ternst@uc.cl>
2024-11-07 17:31:07 -0300

    style: add navbar styles

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -3,15 +3,17 @@ import './NavBar.css'
 
 export default function NavBar() {
     return (
-        <nav className="navbar">
-            <div className="navbar-left">
-                <h1 className="navbar-name">Tomás Ernst</h1>
-            </div>
-            <div className="navbar-right">
-                <Link to="/bucketlist" className="navbar-link">Bucket List</Link>
-                <Link to="/worldmap" className="navbar-link">World Map</Link>
-                <Link to="/minesweeper" className="navbar-link">Minesweeper</Link>
-            </div>
-        </nav>
+        <div className="navbar-container">
+            <nav className="navbar">
+                <div className="navbar-left">
+                    <Link to="/" className="navbar-name">Tomás Ernst</Link>
+                </div>
+                <div className="navbar-right">
+                    <Link to="/bucketlist" className="navbar-link">Bucket List</Link>
+                    <Link to="/worldmap" className="navbar-link">World Map</Link>
+                    <Link to="/minesweeper" className="navbar-link">Minesweeper</Link>
+                </div>
+            </nav>
+        </div>
     );
 }
